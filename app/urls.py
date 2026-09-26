079f6411e5cbd87a0ec8f5bb8a3b9e85b34ce12c
GUSTAVO OLIVEIRA PIRES <gustavo.pires@educacao.senac.rio>
2026-09-25 21:18:11 -0300

    fim aula 11

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('produtos/', views.produtos_view, name='produtos'),
     path('perfil/', views.perfil_view, name='perfil'),
     path('status/', views.status_view, name='status'),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Produto
-
+from .forms import ProdutoForm
 
 def home_view(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 def produtos_view(request):
     lista_produtos = Produto.objects.all()
+    form = ProdutoForm()
 
-    context = {'produtos': lista_produtos}
+    if request.method == 'POST':
+        # Se veio 'produto_id', é uma EDIÇÃO (UPDATE)
+        if 'produto_id' in request.POST:
+            produto = get_object_or_404(Produto, id=request.POST.get('produto_id'))
+            form = ProdutoForm(request.POST, instance=produto)
+        # Se NÃO veio, é um NOVO produto (CREATE)
+        else:
+            form = ProdutoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('produtos')
+
+    context = {
+        'produtos': lista_produtos,
+        'form': form,
+    }
     
-    return render(request,'produtos.html', context)
+    return render(request, 'produtos.html', context)
 
 def perfil_view(request):
     context = {'nome_usuario': 'Gustavo' , 'cargo': 'Instrutor' , 'setor': 'TI'}
